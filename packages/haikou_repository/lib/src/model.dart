a1e5974cacd1f6db558fb89f886a65f48f540237
harry <allthatjazz@outlook.com>
2022-08-14 17:06:41 +0800

    Add mock student repository

---
+++ packages/haikou_repository/lib/src/model.dart
@@ -0,0 +1,15 @@
+class Student {
+  final String name;
+  final int age;
+  final int schoolId;
+  final int gender;
+  final String phone;
+
+  const Student({
+    required this.name,
+    required this.age,
+    required this.schoolId,
+    required this.gender,
+    required this.phone,
+  });
+}
